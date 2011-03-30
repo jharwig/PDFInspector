97f75252068841c2399e8b152cae83d65d19bee7
Jason Harwig <jason.harwig@nearinfinity.com>
2011-03-30 12:26:17 -0400

    Initial Commit

---
+++ PDFObjectInspector/PDFObjectInspector.h
@@ -0,0 +1,15 @@
+//
+//  PDFObjectInspector.h
+//  PDFObjectInspector
+//
+//  Created by Jason Harwig on 3/30/11.
+//  Copyright 2011 Near Infinity Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PDFObjectInspector : NSDocument {
+@private
+}
+
+@end
---
+++ PDFObjectInspector/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PDFObjectInspector
+//
+//  Created by Jason Harwig on 3/30/11.
+//  Copyright 2011 Near Infinity Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
